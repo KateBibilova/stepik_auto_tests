cb9ac29bcb65b38865ccd3c6c076b03bf364d78f
katerina <katerina0310@gmail.com>
2024-03-24 20:11:23 +0200

    added tests for 1.6

---
+++ tests/stepik block 1/test_fail.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+link = "http://suninjuly.github.io/registration2.html"
+
+browser = webdriver.Chrome()
+browser.get(link)
+
+first_name_field = browser.find_element(by=By.CSS_SELECTOR, value='input[placeholder="Input your first name"]')
+first_name_field.send_keys("Kate")
+
+last_name_field = browser.find_element(by=By.CSS_SELECTOR, value='input[placeholder="Input your last name"]')
+last_name_field.send_keys("Bibilova")
+
+email_field = browser.find_element(by=By.CSS_SELECTOR, value='input[placeholder="Input your email"]')
+email_field.send_keys("kate@gmail.com")
+
+button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+button.click()
+
+WebDriverWait(browser, 5).until(
+    EC.visibility_of_element_located((By.TAG_NAME, "h1"))
+)
+
+welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+welcome_text = welcome_text_elt.text
+
+assert "Congratulations! You have successfully registered!" == welcome_text
+
+browser.quit()
